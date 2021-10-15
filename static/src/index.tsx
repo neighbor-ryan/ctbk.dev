293850e3269bb9425287f594e3f4bb69ccd55009
Ryan Williams <ryan@runsascoded.com>
2021-10-15 00:19:25 -0400

    add year lines

--- static/src/index.tsx
+++ static/src/index.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 import { Component, createElement } from "react";
 import Plot from 'react-plotly.js';
 import $ from 'jquery';
-import {OhclData, PlotData} from "plotly.js";
+import {Shape} from "plotly.js";
 // import './index.css';
 
 const workerUrl = new URL(
@@ -12,34 +12,6 @@ const workerUrl = new URL(
     import.meta.url
 );
 const wasmUrl = new URL("sql.js-httpvfs/dist/sql-wasm.wasm", import.meta.url);
-
-// async function load() {
-//   const url = "https://ctbk.s3.amazonaws.com/aggregated/ymrgtb_cd_201306:202109.sqlite";
-//   console.log("Fetching DB:", url);
-//   const worker = await createDbWorker(
-//     [
-//       {
-//         from: "inline",
-//         config: {
-//           serverMode: "full",
-//           url: url,
-//           requestChunkSize: 4096,
-//         },
-//       },
-//     ],
-//     workerUrl.toString(),
-//     wasmUrl.toString()
-//   );
-//
-//   const result = await worker.db.query(`select * from agg limit 10`);
-//
-//   const dataDiv = document.getElementById("data")
-//   if (dataDiv) {
-//       dataDiv.textContent = JSON.stringify(result);
-//   }
-// }
-
-// load();
 
 type State = {
     data: any[] | null
@@ -56,7 +28,7 @@ class App extends Component<any, State> {
             fetch('/assets/plot.json')
                 .then(response => response.json())
                 .then(json => {
-                    console.log("got json:", json);
+                    //console.log("got json:", json);
                     this.setState({json: json,})
                 });
         } else {
@@ -86,12 +58,18 @@ class App extends Component<any, State> {
     constructor(props: any) {
         super(props);
         this.state = { data: null, region: 'JC', json: null, }
+        this.onRegion = this.onRegion.bind(this);
+    }
+
+    onRegion(e: any) {
+        console.log("event:", e.target.value);
     }
 
     render() {
         const state = this.state;
         const data: (null | ({ Month: Date, Count: number, Region: 'JC'|'NYC' }[])) = state['data'];
         const region = state['region'];
+        console.log("render; region:", region);
         const json = state['json']
         if (json) {
             console.log("found json");
@@ -118,34 +96,39 @@ class App extends Component<any, State> {
             const cur = agg.get(key);
             const count = r['Count'];
             if (cur === undefined) {
-                console.log("Month:",month)
+                //console.log("Month:",month)
                 agg.set(key, count);
             } else {
                 agg.set(key, cur + count)
             }
-            // if (!(key in agg)) {
-            //     agg.set(key, 0);
-            // }
-            // const cur: number = agg.get(key)
-            // agg.set(key, cur + count);
         })
         const months: string[] = Array.from(agg.keys());
         const counts: number[] = Array.from(agg.values());
         // const months = data.map((r: any) => r['Month'])
         // const counts = data.map((r: any) => r['Count'])
-        console.log(months, counts)
+        //console.log(months, counts)
 
-        // const plotData: PlotData = {
-        //     x: months,
-        //     y: counts,
-        //     type: 'bar',
-        //     marker: {
-        //         color: '#88aaff',
-        //     },
-        //     // xperiod: "M1",
-        //     // xperiodalignment: "start",
-        //     // fillcolor: '#ff0000', //'#88aaff',
-        // }
+        function vline(year: number): Partial<Shape> {
+            const x: string = `${year-1}-12-20`;
+            return {
+                layer: 'below',
+                type: 'line',
+                x0: x,
+                y0: 0,
+                x1: x,
+                yref: 'paper',
+                y1: 1,//00000,
+                line: {
+                    color: '#555555',
+                    // width: 1.5,
+                    // dash: 'dot'
+                }
+            }
+        }
+
+        const allYears: Array<number> = months.map((m) => new Date(m)).filter((d) => d.getMonth() == 0).map((d) => d.getFullYear());
+        const years: Array<number> = [...new Set(allYears)];
+        const vlines: Array<Partial<Shape>> = years.map(vline);
 
         return (
             <div id="plot">
@@ -161,14 +144,7 @@ class App extends Component<any, State> {
                         },
                     ]}
                     layout={{
-                        // xaxis: {
-                        //
-                        // }
-
-                        //width: '100%',
-                        //width: 320, height: 240,
                         title: 'Citibike Rides By Month',
-                        // yaxis_gridcolor: '#DDDDDD',
                         yaxis: {
                             gridcolor: '#DDDDDD',
                         },
@@ -177,22 +153,14 @@ class App extends Component<any, State> {
                         font: {
                             size: 18,
                         },
-                        shapes: [{
-                            type: 'line',
-                            x0: '2018-01-01',
-                            y0: 0,
-                            x1: '2018-01-01',
-                            yref: 'paper',
-                            y1: 100000,
-                            line: {
-                                color: 'red',
-                                width: 1.5,
-                                dash: 'dot'
-                            }
-                        }],
+                        shapes: vlines,
                     }}
-                    // color_discrete_sequence={['#88aaff']}
                 />
+                <div id="region-controls" onChange={(e: any) => this.setState({ region: e.target.value })}>
+                    <input type="radio" name="region" value="All"></input><label>All</label>
+                    <input type="radio" name="region" value="NYC"></input><label>NYC</label>
+                    <input type="radio" name="region" value="JC"></input><label>JC</label>
+                </div>
             </div>
         );
     }
