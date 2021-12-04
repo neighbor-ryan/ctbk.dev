6ac70d8d702b2e79c10e4b01792c01f58815e59b
Ryan Williams <ryan@runsascoded.com>
2021-12-03 21:33:14 -0500

    trim to just static site files

--- static/sync.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-
-
-aws s3 sync --dryrun \
-  --exclude '*' \
-  --include index.html --include dist/bundle.js --include assets/index.css --include assets/index.css --include assets/citibike.css \
-  . s3://ctbk/static/
