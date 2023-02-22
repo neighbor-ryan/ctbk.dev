521b89aca808f3f157f204008a661ca9b6a99e93
Ryan Williams <ryan@runsascoded.com>
2023-02-22 11:47:00 -0500

    ymrgtb_cd.py cleanup

--- ctbk/ymrgtb_cd.py
+++ ctbk/ymrgtb_cd.py
@@ -1,52 +1,9 @@
 #!/usr/bin/env python
-import dask.dataframe as dd
-
 from ctbk import Monthy
 from ctbk.month_agg_table import MonthAggTable
 from ctbk.util import stderr
 from ctbk.util.df import meta, DataFrame
 from ctbk.util.ym import YM
-
-
-# ymrgtb_cd = read_parquet('s3://ctbk/aggregated/ymrgtb_cd.parquet')
-
-
-# renames = {
-#     'r': ['NYC', 'JC', 'HOB'],
-#     't': ['Annual', 'Daily'],
-#     'b': ['classic_bike', 'docked_bike', 'electric_bike', 'unknown'],
-# }
-# renames = {
-#     k: { v: idx for idx, v in enumerate(vs) }
-#     for k, vs in renames.items()
-# }
-# print(renames)
-#
-# ymr_json2 = (
-#     ymr_json
-#     .rename(columns={
-#         'Region': 'r',
-#         'Year': 'y',
-#         'Month': 'm',
-#         'Gender': 'g',
-#         'User Type': 't',
-#         'Rideable Type': 'b',
-#         'Count': 'c',
-#         'Duration': 'd',
-#     })
-# )
-# ymr_json2 = (
-#     ymr_json2
-#     .assign(
-#         **{
-#             k: ymr_json2[k].apply(lambda v: o[v])
-#             for k, o in renames.items()
-#         },
-#         y=ymr_json2.y.apply(lambda y: y - 2000)
-#     )
-# )
-# print(ymr_json2)
-# ymr_json2.to_json('www/public/assets/ymrgtb_cd.short.json', 'records')
 
 
 class YmrgtbCdJson(MonthAggTable):
