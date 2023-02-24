d415fb3a910a7cd0ceae9f1bbb4e150b697cb8a8
Ryan Williams <ryan@runsascoded.com>
2023-02-24 00:46:16 -0500

    reqs, ymrgtb_cd fix

--- ctbk/ymrgtb_cd.py
+++ ctbk/ymrgtb_cd.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from ctbk import Monthy
 from ctbk.month_agg_table import MonthAggTable
 from ctbk.util.df import meta, DataFrame
 
@@ -7,6 +8,9 @@ class YmrgtbCdJson(MonthAggTable):
     # Used by [`index.tsx`](www/pages/index.tsx) plot
     SRC = 'ctbk/aggregated'
     OUT = 'www/public/assets/ymrgtb_cd.json'
+
+    def url(self, ym: Monthy) -> str:
+        return f'{self.dir}/ymrgtb_cd_{ym}.parquet'
 
     def reduce(self, mapped_dfs: list[DataFrame]) -> DataFrame:
         df = self.dpd.concat(mapped_dfs)
