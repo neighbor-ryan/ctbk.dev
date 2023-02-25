5f7a9f75c6e939a1e467dbe07e21b0d52d76ce7e
Ryan Williams <ryan@runsascoded.com>
2023-02-24 20:30:55 -0500

    uncomment screenshots

--- www/screenshots.js
+++ www/screenshots.js
@@ -3,11 +3,11 @@ const puppeteer = require('puppeteer');
 (async () => {
     const dir = `public/screenshots`
     const screens = {
-        // 'ctbk-rides': { query: '', height: 540, },
-        // 'ctbk-nj': { query: '?r=jh', },
-        // 'ctbk-stations': { query: 'stations?ll=40.732_-74.025&z=13.5&ss=HB102', width: 800, height: 800, selector: '.leaflet-lines-pane svg path' },
-        // 'ctbk-ride-minutes-by-gender': { query: '?y=m&s=g&pct=&g=mf&d=1406-2101', },
-        // 'ctbk-rides-by-user': { query: '?s=u&pct=', },
+        'ctbk-rides': { query: '', height: 540, },
+        'ctbk-nj': { query: '?r=jh', },
+        'ctbk-stations': { query: 'stations?ll=40.732_-74.025&z=13.5&ss=HB102', width: 800, height: 800, selector: '.leaflet-lines-pane svg path' },
+        'ctbk-ride-minutes-by-gender': { query: '?y=m&s=g&pct=&g=mf&d=1406-2101', },
+        'ctbk-rides-by-user': { query: '?s=u&pct=', },
         'ctbk-ebike-minutes': { query: '?y=m&s=b&rt=ce', },
     }
 
