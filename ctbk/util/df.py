65188ac6b2c366dc4366d4aab1604a9ac8f9fd6e
Ryan Williams <ryan@runsascoded.com>
2025-02-09 15:13:23 +0000

    improve error reporting on `to_parquet` failures

--- ctbk/util/df.py
+++ ctbk/util/df.py
@@ -19,13 +19,17 @@ def checkpoint(
     if callable(write_kwargs):
         write_kwargs(df)
     elif fmt == 'pqt':
-        df.to_parquet(url, **write_kwargs)
+        try:
+            df.to_parquet(url, **write_kwargs)
+        except ValueError as e:
+            raise RuntimeError(f"Error saving {url}") from e
     elif fmt == 'csv':
         df.to_csv(url, **write_kwargs)
     elif fmt == 'json':
         df.to_json(url, **write_kwargs)
     else:
         raise ValueError(f"Unrecognized fmt: {fmt}")
+
     if read is Disk:
         read_kwargs = read_kwargs or dict()
         if callable(read_kwargs):
